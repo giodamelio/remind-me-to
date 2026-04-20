9e8283357cda84c1ab2ba96716b3ca4d8d217ca0
Giovanni d'Amelio <gio@damelio.net>
2026-04-20 14:10:41 -0700

    Update devenv configuration

--- devenv.nix
+++ devenv.nix
@@ -7,6 +7,11 @@
 }: {
   languages.rust.enable = true;
 
+  packages = [
+    pkgs.cargo-insta
+    pkgs.cargo-nextest
+  ];
+
   git-hooks.hooks = {
     cargo-check.enable = true;
     rustfmt.enable = true;
@@ -14,7 +19,7 @@
     cargo-test = {
       enable = true;
       name = "cargo-test";
-      entry = "${lib.getExe config.languages.rust.toolchain.cargo} test";
+      entry = "${lib.getExe config.languages.rust.toolchain.cargo} nextest run";
       pass_filenames = false;
     };
   };
